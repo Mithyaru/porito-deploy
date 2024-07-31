adec999e80651a53f27fec7eb12701d3488de873
Vinícius Macêdo <142446793+Mithyaru@users.noreply.github.com>
2024-07-31 14:20:53 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,6 +12,13 @@ app.use(cors({
     credentials: true,
 }))
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE'); // Allow methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization'); // Allow headers
+  next();
+});
+
 app.get('/', (req, res) => {
     let tagLine = req.query.tag
     let gameName = req.query.username
