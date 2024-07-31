ded876772e7c042c3b2818bb095de81fbdbd177b
Vinícius Macêdo <142446793+Mithyaru@users.noreply.github.com>
2024-07-31 19:07:11 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,6 +20,47 @@ res.json('Tentativa 3' + tagLine + gameName)
 }
 )
 
+const apiKey = 'RGAPI-9df4b501-dcaa-434b-ab27-dcf2fc16bab3'
+
+function getPlayerPUUID(tagLine, gameName){
+    return axios.get("https://americas.api.riotgames.com" + "/riot/account/v1/accounts/by-riot-id/" + gameName + '/' + tagLine + "?api_key=" + apiKey)
+        .then(async response => {
+            let accData = response.data
+            return accData
+            
+        }).catch(err => {
+            let erro = err
+            return erro
+            
+        })
+}
+
+
+app.get('/gameUser', async (req, res) => {
+    let tagLine = req.query.tag
+    let gameName = req.query.username
+
+    const PUUIDcall = await getPlayerPUUID(tagLine, gameName)
+    const userCall = 'https://br1.api.riotgames.com' + '/lol/summoner/v4/summoners/by-puuid/' + PUUIDcall.puuid + "?api_key=" + apiKey
+
+    const userInfo = await axios.get(userCall)
+        .then(async response => {
+            const userData = await response.data
+            summonerID = userData.id
+            console.log('con1')
+            console.log(response)
+            return userData
+        })
+        .catch(err => {
+            const erro = 'error'
+            console.log('con2')
+            console.log(erro)
+            return erro 
+        })
+    res.json(userInfo) 
+    //console.log(userInfo)
+})
+
 app.get('/about', (req, res) => res.json('About Page Route'));
 
 
